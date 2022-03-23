dc2ff3a1e89606c0219a8aefcef069c5d0051495
mattiasvillani <mattias.villani@gmail.com>
2022-03-23 19:21:35 +0100

    adding dirichlet distribution

--- R/distributions.R
+++ R/distributions.R
@@ -12,3 +12,25 @@
 rScaledInvChi2 <- function(n, v, tau2){
   return((v*tau2)/rchisq(n, df=v))
 }
+
+#' Simulates from the Dirichlet distribution.
+#'
+#' @param n number of draws
+#' @param alpha vector with hyperparameters
+#' @return n-by-p matrix where each row is a draw.
+#' @export
+#' @examples
+#' library(bayeslearn)
+#' draws = rDirichlet(n = 1000, alpha = c(3,5,10))
+#' colMeans(draws) # Close to theoretical c(3,5,10)/18 = 0.167 0.278 0.556
+rDirichlet <- function(n, alpha){
+  p <- length(alpha) # dimension of the generated vectors
+  thetaDraws <- as.data.frame(matrix(NA, n, p)) # Storage.
+  for (j in 1:p){
+    thetaDraws[,j] <- rgamma(n,alpha[j],1)
+  }
+  for (i in 1:n){
+    thetaDraws[i,] = thetaDraws[i,]/sum(thetaDraws[i,])
+  }
+  return(thetaDraws)
+}
